010d7a3f672f2aaec01429106e60d611bda367b2
solo981 <39235866+chichoargentina@users.noreply.github.com>
2024-10-23 03:51:57 -0300

    Create not-allowed.tsx

---
+++ src/pages/not-allowed.tsx
@@ -0,0 +1,9 @@
+// pages/not-allowed.tsx
+export default function NotAllowed() {
+  return (
+    <div>
+      <h1>Acceso Denegado</h1>
+      <p>Esta aplicación solo puede ser utilizada en los dominios autorizados.</p>
+    </div>
+  );
+}
