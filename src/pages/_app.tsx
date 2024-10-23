9da842203dc635012044263689baa0fad80b5d7a
solo981 <39235866+chichoargentina@users.noreply.github.com>
2024-10-23 09:06:44 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,3 +11,24 @@ export default function App({ Component, pageProps }: AppProps) {
     </>
   );
 }
+
+
+// agrega en pages/_app.tsx
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Comprobar si la aplicación está siendo cargada en un iframe
+    if (window.self === window.top) {
+      // Si no está en un iframe, redirigir a la página de acceso denegado
+      router.push('/not-allowed');
+    }
+  }, [router]);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
